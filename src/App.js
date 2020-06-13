117cf67b3308df2bc69b468edcb4002f18b4a989
NektoUa <illyabarkov@gmail.com>
2020-06-13 19:55:22 +0300

    add todo and account

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Login from './pages/login';
 import Signup from './pages/signup';
+import Home from './pages/home';
 
 function App() {
     return (
@@ -9,7 +10,7 @@ function App() {
         <Router>
             <div>
                 <Switch>
-                    {/* <Route exact path="/" component={home} /> */}
+                    <Route exact path="/" component={Home} />
                     <Route exact path="/login" component={Login} />
                     <Route exact path="/signup" component={Signup} />
                 </Switch>
